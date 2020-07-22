3db307d649fc80cc2f5d4539ce93bafc86553076
Asmit <ViAsmit>
2020-07-22 19:18:31 +0530

    Completed naive_bayes and svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -18,13 +18,31 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+# features_train = features_train[:len(features_train)/100]
+# labels_train =  labels_train[:len(labels_train)/100]
 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
+clf = svm.SVC(kernel='rbf', C=10000.0)
+print "Training..."
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time: ", round(time()-t0, 3), "s"
 
+print "predicting..."
+t0 = time()
+pred = clf.predict(features_test)
+print "predict time: ", round(time()-t0, 3), "s"
+
+acc = accuracy_score(pred, labels_test)
+print(acc)
+
+print(len(pred))
+count = len(pred[pred==1])
+print count
 
 #########################################################
 ### your code goes here ###
 
 #########################################################
-
-
